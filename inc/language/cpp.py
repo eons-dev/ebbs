0bf3358aa94a4462ae5c4ab80dcb320aed96adbe
eons <eons@eons.dev>
2021-11-22 11:35:58 -0800

    Prepares cpp building for publishing shared libraries.

--- inc/language/cpp.py
+++ inc/language/cpp.py
@@ -1,12 +1,16 @@
 import os
 import logging
+import shutil
+from distutils.file_util import copy_file
+from distutils.dir_util import copy_tree, mkpath
 from ebbs import Builder
 
-#Class name is what is used at cli, so we defy convention here in favor of ease-of-use.
+
+# Class name is what is used at cli, so we defy convention here in favor of ease-of-use.
 class cpp(Builder):
     def __init__(self, name="C++ Builder"):
         super().__init__(name)
-        
+
         self.supportedProjectTypes.append("lib")
         self.supportedProjectTypes.append("bin")
         self.supportedProjectTypes.append("test")
@@ -20,14 +24,29 @@ class cpp(Builder):
             ".so"
         ]
 
-    #Required Builder method. See that class for details.
+    # Required Builder method. See that class for details.
     def Build(self):
+        self.buildPath = os.path.join(self.rootPath, self.buildPath)
+        if (os.path.exists(self.buildPath)):
+            logging.info(f"DELETING {self.buildPath}")
+            shutil.rmtree(self.buildPath)
+        mkpath(self.buildPath)
         os.chdir(self.buildPath)
+        self.packagePath = os.path.join(self.buildPath, self.projectName)
+        mkpath(self.packagePath)
+
+        logging.debug(f"Building in {self.buildPath}")
+        logging.debug(f"Packaging in {self.packagePath}")
+
         self.GenCMake()
-        self.CMake()
+        self.CMake(".")
         self.Make()
 
-    def get_cxx_files (self, directory, seperator=" "):
+        # include header files with libraries
+        if (self.projectType in ["lib"]):
+            copy_tree(self.incPath, self.packagePath)
+
+    def get_cxx_files(self, directory, seperator=" "):
         ret = ""
         for root, dirs, files in os.walk(directory):
             for f in files:
@@ -36,7 +55,7 @@ class cpp(Builder):
                     # logging.info(f"    {os.path.join(root, f)}")
                     ret += f"{os.path.join(root, f)}{seperator}"
         return ret[:-1]
-    
+
     def get_libs(self, directory, seperator=" "):
         ret = ""
         for file in os.listdir(directory):
@@ -48,11 +67,14 @@ class cpp(Builder):
         return ret[:-1]
 
     def GenCMake(self):
-        #Write our cmake file
-        cmake_open = '''
+        # Write our cmake file
+        cmake_open = f'''
 cmake_minimum_required (VERSION 3.1.1)
 set (CMAKE_CXX_STANDARD 11)
-include_directories(${CMAKE_SOURCE_DIR}/../inc)
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY {self.packagePath})
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY {self.packagePath})
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY {self.packagePath})
+include_directories({self.incPath})
 '''
 
         cmake_file = open("CMakeLists.txt", "w")
@@ -61,35 +83,38 @@ include_directories(${CMAKE_SOURCE_DIR}/../inc)
 
         if (self.projectType in ["bin", "test"]):
             logging.info("Addind binary specific code")
-            
-            cmake_file.write (f"add_executable ({self.projectName} {self.get_cxx_files ('../src')})\n")
-            cmake_file.write ('''
-include_directories(${CMAKE_SOURCE_DIR}/../lib)
+
+            cmake_file.write(f"add_executable ({self.projectName} {self.get_cxx_files(self.srcPath)})\n")
+            cmake_file.write(f'''
+include_directories({self.libPath})
 set(THREADS_PREFER_PTHREAD_FLAG ON)
 find_package(Threads REQUIRED)
 ''')
-            cmake_file.write (f"target_link_directories({self.projectName} " + "PUBLIC ${CMAKE_SOURCE_DIR}/../lib)\n")
-            cmake_file.write (f"target_link_libraries({self.projectName} Threads::Threads {self.get_libs('../lib')})")
+            cmake_file.write(f"target_link_directories({self.projectName} " + f"PUBLIC {self.libPath}))\n")
+            cmake_file.write(
+                f"target_link_libraries({self.projectName} Threads::Threads {self.get_libs(self.libPath)})")
 
         if (self.projectType in ["lib", "mod"]):
-            logging.info("Addind library specific code")
+            logging.info("Adding library specific code")
 
-            #TODO: support windows install targets
-            src_path = "/usr/local/lib"
-            inc_path = "/usr/local/include/{self.projectName}"
+            # #TODO: support windows install targets
+            installSrcPath = "/usr/local/lib"
+            installIncPath = "/usr/local/include/{self.projectName}"
 
-            cmake_file.write (f"add_library ({self.projectName} STATIC {self.get_cxx_files('../src')})\n")
-            cmake_file.write (f"set_target_properties({self.projectName} PROPERTIES PUBLIC_HEADER \"{self.get_cxx_files('../inc',';')}\")\n")
-            cmake_file.write (f"INSTALL(TARGETS {self.projectName} LIBRARY DESTINATION {src_path} PUBLIC_HEADER DESTINATION {inc_path})\n")
+            cmake_file.write(f"add_library ({self.projectName} SHARED {self.get_cxx_files(self.srcPath)})\n")
+            cmake_file.write(
+                f"set_target_properties({self.projectName} PROPERTIES PUBLIC_HEADER \"{self.get_cxx_files(self.incPath, ';')}\")\n")
+            cmake_file.write(
+                f"INSTALL(TARGETS {self.projectName} LIBRARY DESTINATION {installSrcPath} PUBLIC_HEADER DESTINATION {installIncPath})\n")
 
         cmake_close = '''
 '''
-        cmake_file.write (f"{cmake_close}")
+        cmake_file.write(f"{cmake_close}")
 
         cmake_file.close()
 
-    def CMake(self):
-        self.RunCommand("cmake .")
+    def CMake(self, path):
+        self.RunCommand(f"cmake {path}")
 
     def Make(self):
         self.RunCommand("make")
