1dcb244648bf33da21dc14d1f7ed38078052bc45
eons <eons@eons.dev>
2022-01-13 06:26:43 -0800

    Adds support for "run_when" specification.

--- src/EBBS.py
+++ src/EBBS.py
@@ -20,25 +20,31 @@ class EBBS(e.Executor):
         super().AddArgs()
         self.argparser.add_argument('path', type = str, nargs='?', metavar = '/project/', help = 'path to project folder', default = '.')
         self.argparser.add_argument('-i', '--build-in', type = str, metavar = 'build', help = 'name of folder to use inside project (/project/build/)', default = 'build', dest = 'build_in')
-        self.argparser.add_argument('-b','--build', type = str, metavar = 'cpp', help = 'build of files to build', dest = 'lang')
+        self.argparser.add_argument('-b','--build', type = str, metavar = 'cpp', help = 'build of files to build', dest = 'builder')
+        self.argparser.add_argument('-e','--event', type = str, action='append', nargs='*', metavar = 'release', help = 'what is going on that triggered this build?', dest = 'events')
+
 
     #Override of eons.Executor method. See that class for details
     def ParseArgs(self):
         super().ParseArgs()
 
-        if (not self.args.lang):
+        self.events = set()
+        if (self.args.events is not None):
+            [[self.events.add(e) for e in l] for l in self.args.events]
+
+        if (not self.args.builder):
             logging.debug("No build specified. Assuming build pipeline is written in build.json.")
 
     #Override of eons.Executor method. See that class for details
     def UserFunction(self, **kwargs):
         super().UserFunction(**kwargs)
-        self.Execute(self.args.lang, self.args.path, self.args.build_in, **self.extraArgs)
+        self.Execute(self.args.builder, self.args.path, self.args.build_in, self.events, **self.extraArgs)
 
     #Run a build script.
-    def Execute(self, build, path, build_in, **kwargs):
+    def Execute(self, build, path, build_in, events, **kwargs):
         if (not build):
             builder = Builder("EMPTY")
         else:
             builder = self.GetRegistered(build, "build")
-        logging.debug(f"Executing {build} in {path}/{build_in} with additional args: {kwargs}")
-        builder(executor=self, path=path, build_in=build_in, **kwargs)
+        logging.debug(f"Executing {build} in {path}/{build_in} with events {events} and additional args: {kwargs}")
+        builder(executor=self, path=path, build_in=build_in, events=events, **kwargs)
